ce5bc9159096494a3deaddd6485690eb05a18432
Mohammed ismayil <mohammedismayil10@gmail.com>
2026-08-10 17:13:39 +0530

    GCD concurrent thread example

--- ExpenseTracker/SampleFiles/Topics/GCDImageFetchView.swift
+++ ExpenseTracker/SampleFiles/Topics/GCDImageFetchView.swift
@@ -16,6 +16,7 @@ struct GCDImage: Identifiable {
 struct GCDImageFetchView: View {
     
     @State var images: [GCDImage] = [GCDImage(id: "1", isLoaded: false, name: "1"),GCDImage(id: "2", isLoaded: false, name: "1"),GCDImage(id: "3", isLoaded: false, name: "1"),GCDImage(id: "4", isLoaded: false, name: "1"),GCDImage(id: "5", isLoaded: false, name: "1")]
+    @State var completedCount: Int = 0
     var body: some View {
         VStack {
             
@@ -25,7 +26,7 @@ struct GCDImageFetchView: View {
                         Text(image.name)
                         Color(.orange).frame(width: 100, height: 100).containerShape(.buttonBorder)
                         if image.isLoaded {
-                            Image("image\(image.name)").resizable().frame(height: 100)
+                            Image("image\(image.name)").resizable().frame(width: 100,height: 100)
                         } else {
                             ProgressView()
                         }
@@ -33,41 +34,20 @@ struct GCDImageFetchView: View {
                 }
                 
             }
+            Text("Total loaded images: \(completedCount)")
             Button("Start Processing") {
                 // GCD code will come here
                 let queue = DispatchQueue(label: "image", attributes: .concurrent)
-                queue.async {
-                    print("1st image download")
-                    Thread.sleep(forTimeInterval: .random(in: 0...3))
-                    images[0].isLoaded = true
-                    print("1st image loaded")
-                }
-                queue.async {
-                    print("2st image download")
-                    Thread.sleep(forTimeInterval: .random(in: 0...3))
-                    images[1].isLoaded = true
-                    print("2st image loaded")
-                }
-                queue.async {
-                    print("3st image download")
-                    Thread.sleep(forTimeInterval: .random(in: 0...3))
-                    DispatchQueue.main.async {
-                        images[2].isLoaded = true
-                        print("3st image loaded")
+                for i in 0..<images.count {
+                    queue.async {
+                        print("\(i+1) image download")
+                        Thread.sleep(forTimeInterval: .random(in: 0...3))
+                        DispatchQueue.main.async {
+                            images[i].isLoaded = true
+                            completedCount += 1
+                            print("\(i+1)st image loaded")
+                        }
                     }
-                   
-                }
-                queue.async {
-                    print("4st image download")
-                    Thread.sleep(forTimeInterval: .random(in: 0...3))
-                    images[3].isLoaded = true
-                    print("4st image loaded")
-                }
-                queue.async {
-                    print("5st image download")
-                    Thread.sleep(forTimeInterval: .random(in: 0...3))
-                    images[4].isLoaded = true
-                    print("5st image loaded")
                 }
             }
             
