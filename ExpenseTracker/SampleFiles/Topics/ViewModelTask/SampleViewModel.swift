e7ae35e0107ea952317635e62e4e9fe46e880c66
Mohammed ismayil <mohammedismayil10@gmail.com>
2026-08-31 21:52:06 +0530

    SampleViewModel

--- ExpenseTracker/SampleFiles/Topics/ViewModelTask/SampleViewModel.swift
+++ ExpenseTracker/SampleFiles/Topics/ViewModelTask/SampleViewModel.swift
@@ -10,45 +10,73 @@ import Combine
 import SwiftUI
 
 struct SampleUsersListView: View {
-    @StateObject var viewModel: SampleViewModel = SampleViewModel(repository: SampleUserRepository(service: MockAPIService()))
+    @StateObject var viewModel: SampleViewModel = SampleViewModel(repository: SampleUserRepository(service: FetchUserAPIService()))
     var body: some View {
-        if viewModel.error != nil {
-            Text("\(viewModel.error?.localizedDescription)")
-        } else {
-            List(viewModel.userData) { user in
-                Text(user.title)
-            }.onAppear {
-                Task {
-                    await viewModel.fetchUsers()
+        Group {
+            if viewModel.fetchState == .fetchError {
+                Text("\(viewModel.error?.localizedDescription)")
+            }else if viewModel.fetchState == .initialLoading {
+                ProgressView()
+            } else {
+                List {
+                    ForEach(viewModel.userData) { user in
+                        Text(user.name).onAppear {
+                            Task {
+                                await viewModel.fetchNextUsers()
+                            }
+                        }
+                    }
+                    if viewModel.fetchState == .paginationLoading {
+                        HStack {
+                            Spacer()
+                            ProgressView()
+                            Spacer()
+                        }
+                    }
+                    
                 }
                 
             }
         }
+        .task {
+            await viewModel.fetchUsers()
+        }
+        
+    }
+}
+
+struct SampleUserResponse: Decodable, Identifiable {
+    var id: String
+    var userList: [SampleUser] = []
+    var isNextPageAvailable: Bool = false
+    
+    init(userList: [SampleUser], isNextPageAvailable: Bool) {
+        self.userList = userList
+        self.isNextPageAvailable = isNextPageAvailable
+        self.id = UUID().uuidString
     }
 }
 
 struct SampleUser: Decodable, Identifiable {
     var id: Int
-    var priority: Int
-    var title: String
-    init(id: Int, priority: Int, title: String) {
+    var name: String
+    init(id: Int, name: String) {
         self.id = id
-        self.priority = priority
-        self.title = title
+        self.name = name
     }
 }
 
 protocol UserServiceProtocol {
-    func fetchUsers() async throws -> [SampleUser]
+    func fetchUsers(pageNumber: Int) async throws -> SampleUserResponse
 }
 
 protocol UserRepositoryProtocol {
-    func fetchUsers() async throws -> [SampleUser]
+    func fetchUsers(pageNumber: Int) async throws -> SampleUserResponse
 }
 
 final class SampleUserRepository: UserRepositoryProtocol {
-    func fetchUsers() async throws -> [SampleUser] {
-        try await service.fetchUsers()
+    func fetchUsers(pageNumber: Int) async throws -> SampleUserResponse {
+        try await service.fetchUsers(pageNumber: pageNumber)
     }
     
     var service: UserServiceProtocol
@@ -59,29 +87,36 @@ final class SampleUserRepository: UserRepositoryProtocol {
     
 }
 final class FetchUserAPIService: UserServiceProtocol {
-    func fetchUsers() async throws -> [SampleUser] {
-        guard let url = URL(string: "sample") else {
-            return []
+    func fetchUsers(pageNumber: Int) async throws -> SampleUserResponse {
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/users?_page=\(pageNumber)&_limit=2") else {
+            return SampleUserResponse(userList: [], isNextPageAvailable: false)
         }
         do {
             let (data, response) = try await URLSession.shared.data(from: url)
-            guard let response  = response as? HTTPURLResponse else {
+            guard let _  = response as? HTTPURLResponse else {
                 throw FetchUserError.noResponse
             }
-            guard let decoder = try? JSONDecoder().decode([SampleUser].self, from: data) else {
+            guard let list = try? JSONDecoder().decode([SampleUser].self, from: data) else {
                 throw FetchUserError.decodeError
             }
-            return decoder
+            return SampleUserResponse(userList: list, isNextPageAvailable: pageNumber <= 4)
         } catch {
             throw FetchUserError.noInternet
         }
         
     }
     
-    
 }
+
+@MainActor
 class SampleViewModel: ObservableObject {
     @Published var userData: [SampleUser] = []
+    
+    @Published var pageNumber: Int = 1
+    
+    @Published var fetchState: FetchState = .idle
+    
+    @Published var isNextPageNeeded: Bool = false
     
     @Published var error: Error?
     
@@ -93,25 +128,53 @@ class SampleViewModel: ObservableObject {
     
     func fetchUsers() async {
         do {
-            let users = try await repository.fetchUsers()
-            userData = users.sorted {
-                $0.priority > $1.priority
-            }
+            fetchState = pageNumber == 1 ? .initialLoading : .paginationLoading
+            let users = try await repository.fetchUsers(pageNumber: pageNumber)
+            userData.append(contentsOf: users.userList)
+            isNextPageNeeded = users.isNextPageAvailable
+            fetchState = .completed
         } catch {
             self.error = error
+            fetchState = .fetchError
+        }
+    }
+    
+    func fetchNextUsers() async {
+        do {
+            guard fetchState != .paginationLoading, isNextPageNeeded else {
+                return
+            }
+            fetchState = .paginationLoading
+            try await Task.sleep(for: .seconds(2))
+            let users = try await repository.fetchUsers(pageNumber: pageNumber + 1)
+            userData.append(contentsOf: users.userList)
+            pageNumber += 1
+            isNextPageNeeded = users.isNextPageAvailable
+            fetchState = .completed
+        } catch {
+            self.error = error
+            fetchState = .fetchError
         }
     }
 }
 
 final class MockAPIService: UserServiceProtocol {
-    func fetchUsers() async throws -> [SampleUser] {
+    func fetchUsers(pageNumber: Int) async throws -> SampleUserResponse {
         do {
             try await Task.sleep(for: .seconds(3))
-            return [SampleUser(id: 1, priority: 1, title: "john"),SampleUser(id: 2, priority: 2, title: "peter")]
+            return SampleUserResponse(userList: [SampleUser(id: 1, name: "john"),SampleUser(id: 2, name: "peter")], isNextPageAvailable: false)
         } catch {
             throw FetchUserError.noResponse
         }
     }
+}
+
+enum FetchState {
+    case idle
+    case initialLoading
+    case paginationLoading
+    case completed
+    case fetchError
 }
 
 enum FetchUserError: Error {
